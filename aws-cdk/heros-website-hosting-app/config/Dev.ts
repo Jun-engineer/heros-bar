cf45bd85068c4af8d49a5cd30088b09a35cb6c78
jun <jun.drums.1102@gmail.com>
2024-09-17 21:09:21 +0900

    add config file & test files

---
+++ aws-cdk/heros-website-hosting-app/config/Dev.ts
@@ -0,0 +1,11 @@
+// @(#)Dev.ts
+
+const system: string = 'heros-web';
+const env: string = 'dev';
+
+export const common = {
+  account: 'xxxxxxxxxxxx',
+  region: 'ap-northeast-1',
+  systemName: system,
+  environment: env,
+};
